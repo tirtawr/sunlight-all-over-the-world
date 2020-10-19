671ed14821833fd0ad61b2813b787ed4c00520f6
Tirta Rachman <tirtawening@gmail.com>
2020-10-19 16:16:34 -0400

    Update home page

---
+++ docs/precache-manifest.6aa2de6ad1567c42e390473aea48945b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b11999446f7d3c97c335f461ea51f3e",
+    "url": "/sunlight-all-over-the-world/index.html"
+  },
+  {
+    "revision": "9007150ed7195c221af5",
+    "url": "/sunlight-all-over-the-world/static/css/2.d9df7b93.chunk.css"
+  },
+  {
+    "revision": "4bff4fdf41285421228c",
+    "url": "/sunlight-all-over-the-world/static/css/main.ea7251d2.chunk.css"
+  },
+  {
+    "revision": "9007150ed7195c221af5",
+    "url": "/sunlight-all-over-the-world/static/js/2.1a7d059a.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/sunlight-all-over-the-world/static/js/2.1a7d059a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bff4fdf41285421228c",
+    "url": "/sunlight-all-over-the-world/static/js/main.af925b41.chunk.js"
+  },
+  {
+    "revision": "dc5743cd96cde7fba124",
+    "url": "/sunlight-all-over-the-world/static/js/runtime-main.f21a386f.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c32df10691e09aa7b958676f7de92ccd.js"
+  "/sunlight-all-over-the-world/precache-manifest.6aa2de6ad1567c42e390473aea48945b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sunlight-all-over-the-world/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
