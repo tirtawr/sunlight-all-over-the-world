ae4d355a4cecc22749c1b94d3986c21dd6d87b08
Tirta Rachman <tirtawening@gmail.com>
2020-10-19 16:10:13 -0400

    Update build

---
+++ docs/precache-manifest.c32df10691e09aa7b958676f7de92ccd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde1337834b1d825626eb7041f87e49b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a220a1b6906b507ea4b",
+    "url": "/static/css/2.d9df7b93.chunk.css"
+  },
+  {
+    "revision": "796e3351a37dfa5cfdbf",
+    "url": "/static/css/main.ea7251d2.chunk.css"
+  },
+  {
+    "revision": "2a220a1b6906b507ea4b",
+    "url": "/static/js/2.7d843fdd.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.7d843fdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796e3351a37dfa5cfdbf",
+    "url": "/static/js/main.105a1f3f.chunk.js"
+  },
+  {
+    "revision": "b08a902e6c3de7e7e425",
+    "url": "/static/js/runtime-main.1be37cd0.js"
+  }
+]);
